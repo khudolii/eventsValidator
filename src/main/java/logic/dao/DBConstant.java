5bcd83026673af92f819b1f035a775b3e6186f6c
Khudolii Yevhenii <hudoliy.e@gmail.com>
2021-03-27 17:41:19 +0200

    Delete DBConstant.java

--- src/main/java/logic/dao/DBConstant.java
+++
@@ -1,7 +0,0 @@
-package logic.dao;
-
-public class DBConstant {
-    static final String DB_URL = "jdbc:postgresql://10.10.1.10:5434/trackensure";
-    static final String USER_DB = "postgres";
-    static final String PASS_DB = "postgresTE!";
-}
